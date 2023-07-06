332c885ebba866cc77ec190316774bf28b541479
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-07-06 11:31:13 +0100

    hide API_key

--- src/features/stories/storiesSlice.js
+++ src/features/stories/storiesSlice.js
@@ -20,7 +20,9 @@ export const getStories = createAsyncThunk(
 
     try {
       const resp = await axios.get(
-        `https://newsapi.org/v2/everything?q=${query}&language=en&page=${page}&pageSize=${pageSize}&apiKey=aa15640c4b6149b4a67fc925aee56b21`
+        `https://newsapi.org/v2/everything?q=${query}&language=en&page=${page}&pageSize=${pageSize}&apiKey=${
+          import.meta.env.VITE_API_KEY
+        }`
       );
       const { articles, totalResults } = resp.data;
       const totalPages = Math.ceil(totalResults / pageSize);
@@ -37,7 +39,9 @@ export const HeadLines = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const resp = await axios.get(
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=aa15640c4b6149b4a67fc925aee56b21"
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+          import.meta.env.VITE_API_KEY
+        }`
       );
       const { articles } = resp.data;
 
